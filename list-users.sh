fe49b20fc27a2b5c9ca7279deb7b99ee30a23bf4
AdnanAhmad7065 <adnanahmad31033@gmail.com>
2025-04-11 17:59:45 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-helper()
+# Function to check required arguments
+function helper {
+    expected_cmd_args=2
+    if [ $# -ne $expected_cmd_args ]; then
+        echo "Please run the script with the required arguments."
+        echo "Usage: ./script.sh <repo_owner> <repo_name>"
+        exit 1
+    fi
+}
 
 # GitHub API URL
 API_URL="https://api.github.com"
 
-# GitHub username and personal access token
+# GitHub username and personal access token (set as env variables)
 USERNAME=$username
 TOKEN=$token
 
-# User and Repository information
+# User and Repository information from script arguments
 REPO_OWNER=$1
 REPO_NAME=$2
 
-# Function to make a GET request to the GitHub API
+# Function to call GitHub API with GET
 function github_api_get {
     local endpoint="$1"
     local url="${API_URL}/${endpoint}"
-
-    # Send a GET request to the GitHub API with authentication
     curl -s -u "${USERNAME}:${TOKEN}" "$url"
 }
 
-# Function to list users with read access to the repository
+# Function to list users with read access
 function list_users_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
-
-    # Fetch the list of collaborators on the repository
     collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
 
-    # Display the list of collaborators with read access
     if [[ -z "$collaborators" ]]; then
         echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
     else
@@ -38,14 +41,8 @@ function list_users_with_read_access {
     fi
 }
 
-function helper {
- expected_cmd_args=2
- if [ $# -ne $expected_cmd_args]; then
-  echo "please execute the script with required cmd args"
-  echo "asd"
-  }
-
-# Main script
-
+# Main
+helper "$@"
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
+
